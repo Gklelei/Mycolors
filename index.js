71549dcc7a61d0e60a05e41427be521a9fc71634
GIDEON LELEI <71279121+Gklelei@users.noreply.github.com>
2024-02-08 20:09:45 +0300

    linked js to html page

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('form').onsubmit = function(){
+        const btn = document.querySelector("#color").value;
 
+        if(btn === "Red"){
+            alert(`THE COLOR IS ${btn}`);
+        }
+        else{
+            alert('Select another color');
+        }
+    }
+})
