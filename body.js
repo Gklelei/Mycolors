c09d6d72667f1702d91cf86e313b71cc20ba1806
GIDEON LELEI <71279121+Gklelei@users.noreply.github.com>
2024-02-10 13:08:49 +0300

    added bg changer

---
+++ body.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('form').onsubmit = (event) => {
+        event.preventDefault();
+        var btncolor = document.querySelector('#color').value;
+        const txtcolor = document.querySelector(`option[value="${btncolor}"]`).dataset.color;
+
+        if (btncolor) {
+            document.body.style.backgroundColor = txtcolor;
+        }
+    }
+});
