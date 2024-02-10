5de5affb1215ab25973fa59befe3b84e485d172e
GIDEON LELEI <71279121+Gklelei@users.noreply.github.com>
2024-02-10 13:01:21 +0300

    Added js functionality to change the colors

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('form').onsubmit = function(){
-        const btn = document.querySelector("#color").value;
-        if(btn === "Red"){
-            alert(`THE COLOR IS ${btn}`);
-        }
-       else if(btn === "Green"){
-            alert(`THE COLOR IS ${btn}`);
-        }
-        if(btn === "Blue"){
-            alert(`THE COLOR IS ${btn}`);
-        }
-        if(btn === "Yellow"){
-            alert(`THE COLOR IS ${btn}`);
-        }
-        if(btn === "Black"){
-            alert(`THE COLOR IS ${btn}`);
-        }
-        if(btn === "White"){
-            alert(`THE COLOR IS ${btn}`);
+document.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('form').onsubmit = (event)=>{
+        event.preventDefault();
+        var btncolor =document.querySelector('#color').value;
+        const txtcolor = document.querySelector(`option[value="${btncolor}"]`).dataset.color;
+        if (btncolor) {
+            document.querySelector('#myh1').style.color =btncolor;
         }
     }
 })
